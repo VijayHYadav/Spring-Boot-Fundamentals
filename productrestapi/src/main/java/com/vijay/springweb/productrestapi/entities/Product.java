046b6c5d0ac821a6b63527a1457985a843ae1105
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-26 04:46:45 +0530

    006 Mark with JPA annotations

--- productrestapi/src/main/java/com/vijay/springweb/productrestapi/entities/Product.java
+++ productrestapi/src/main/java/com/vijay/springweb/productrestapi/entities/Product.java
@@ -1,7 +1,15 @@
 package com.vijay.springweb.productrestapi.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
