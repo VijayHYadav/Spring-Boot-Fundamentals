78fa33522a4363bea2a718b4afdf8ac996955a08
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-26 04:56:59 +0530

    008 Get Single Product

--- productrestapi/src/main/java/com/vijay/springweb/productrestapi/controllers/ProductRestController.java
+++ productrestapi/src/main/java/com/vijay/springweb/productrestapi/controllers/ProductRestController.java
@@ -3,6 +3,7 @@ package com.vijay.springweb.productrestapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class ProductRestController {
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public List<Product> getProducts () {
         return repository.findAll();
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    public Product getProduct (@PathVariable("id") int id) {
+        return repository.findById(id).get();
     }
 }
