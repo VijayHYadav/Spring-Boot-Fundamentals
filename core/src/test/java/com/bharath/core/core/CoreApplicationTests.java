9677f08dd0bde70f75f5802c842262fc92b24851
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-25 20:32:40 +0530

    006 Testing

--- core/src/test/java/com/bharath/core/core/CoreApplicationTests.java
+++ core/src/test/java/com/bharath/core/core/CoreApplicationTests.java
@@ -1,13 +1,22 @@
 package com.bharath.core.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bharath.core.core.service.PaymentService;
+
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class CoreApplicationTests {
 
+	@Autowired
+	PaymentService service;
+
 	@Test
-	void contextLoads() {
+	void testDependencyInjection() {
+		assertNotNull(service);
 	}
 
 }
