7334d2d5090b0395da8a5d9a20d2fca78d9c7b8b
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-25 21:41:56 +0530

    007 Testing

--- springdatajpa/src/test/java/com/vijay/springdatajpa/springdatajpa/SpringdatajpaApplicationTests.java
+++ springdatajpa/src/test/java/com/vijay/springdatajpa/springdatajpa/SpringdatajpaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.vijay.springdatajpa.springdatajpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vijay.springdatajpa.springdatajpa.entities.Student;
+import com.vijay.springdatajpa.springdatajpa.repos.StudentRepository;
+
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class SpringdatajpaApplicationTests {
 
+	@Autowired
+	private StudentRepository repository;
+
 	@Test
-	void contextLoads() {
+	void testSaveStudent() {
+		Student student = new Student();
+		student.setId(1);
+		student.setName("vijay");
+		student.setTestScore(100);
+		repository.save(student);
+
+		Student savedStudent = repository.findById(1l).get();
+		assertNotNull(savedStudent);
 	}
 
 }
