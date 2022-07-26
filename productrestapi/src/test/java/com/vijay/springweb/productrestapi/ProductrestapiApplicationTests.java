559d62448c7638e815777fab8c80acd005faa0ce
VijayHYadav <vizzyadav1997@gmail.com>
2022-07-26 07:12:18 +0530

    002 RestTemplate GET method

--- productrestapi/src/test/java/com/vijay/springweb/productrestapi/ProductrestapiApplicationTests.java
+++ productrestapi/src/test/java/com/vijay/springweb/productrestapi/ProductrestapiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.vijay.springweb.productrestapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import com.vijay.springweb.productrestapi.entities.Product;
 
 @SpringBootTest
 class ProductrestapiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testGetProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+		assertNotNull(product);
+		assertEquals("IPhone", product.getName());
 	}
 
 }
