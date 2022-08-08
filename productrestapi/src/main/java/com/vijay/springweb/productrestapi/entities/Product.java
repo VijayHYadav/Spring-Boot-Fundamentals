66563a3dd84434274ba16455ee3f675c048def4e
VijayHYadav <vizzyadav1997@gmail.com>
2022-08-08 06:47:54 +0530

    005 Enable and Use Caching

--- productrestapi/src/main/java/com/vijay/springweb/productrestapi/entities/Product.java
+++ productrestapi/src/main/java/com/vijay/springweb/productrestapi/entities/Product.java
@@ -1,12 +1,14 @@
 package com.vijay.springweb.productrestapi.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Product {
+public class Product implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
