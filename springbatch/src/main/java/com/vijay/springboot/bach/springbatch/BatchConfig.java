252c829f433d646064fdd73e429af3f5b2fa6aeb
VijayHYadav <vizzyadav1997@gmail.com>
2022-08-11 01:40:16 +0530

    007 Configure the Step

--- springbatch/src/main/java/com/vijay/springboot/bach/springbatch/BatchConfig.java
+++ springbatch/src/main/java/com/vijay/springboot/bach/springbatch/BatchConfig.java
@@ -1,15 +1,30 @@
 package com.vijay.springboot.bach.springbatch;
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfig {
-    
+
+    private StepBuilderFactory sbf;
+
+    public Step step() {
+        return sbf.get("step-1")
+            .<String, String>chunk(1)
+            .reader(reader())
+            .writer(writer())
+            .processor(processor())
+            .build();
+    }
+
+    @Bean
     public Reader reader() {
         return new Reader();
     }
 
+    @Bean
     public Writer writer() {
         return new Writer();
     }
@@ -19,6 +34,7 @@ public class BatchConfig {
         return new Processor();
     }
 
+    @Bean
     public MyJobListener listener() {
         return new MyJobListener();
     }
