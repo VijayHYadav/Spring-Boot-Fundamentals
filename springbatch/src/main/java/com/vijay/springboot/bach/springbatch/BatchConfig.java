4ca9b1c4c6619aaabf1096771bb98f3d8afd7930
VijayHYadav <vizzyadav1997@gmail.com>
2022-08-11 01:45:56 +0530

    008 Configure the Job

--- springbatch/src/main/java/com/vijay/springboot/bach/springbatch/BatchConfig.java
+++ springbatch/src/main/java/com/vijay/springboot/bach/springbatch/BatchConfig.java
@@ -1,15 +1,32 @@
 package com.vijay.springboot.bach.springbatch;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfig {
 
+    @Autowired
     private StepBuilderFactory sbf;
 
+    @Autowired
+    private JobBuilderFactory jbf;
+
+    @Bean
+    public Job job() {
+        return jbf.get("Job1")
+            .incrementer(new RunIdIncrementer())
+            .listener(listener())
+            .start(step()).build();
+    }
+
+    @Bean
     public Step step() {
         return sbf.get("step-1")
             .<String, String>chunk(1)
